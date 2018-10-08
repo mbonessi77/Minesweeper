fc9c37485b331e292f790e88639a4da708f4d226
Michael Bonessi <mbonessi@uncc.edu>
2018-10-07 23:04:03 -0400

    Clickable Tiles

---
+++ Mines/Assets/Scripts/TileManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    //Variable declaration
+    [SerializeField] private GameObject tile;
+
+	// Use this for initialization
+	void Start ()
+    {
+		for(int i = 0; i < 14; i++)
+        {
+            for(int k = 0; k < 8; k++)
+            {
+                Instantiate(tile, new Vector2(i, k), Quaternion.identity);
+            }
+        }
+	}
+}
---
+++ Mines/Assets/Scripts/TileScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileScript : MonoBehaviour
+{
+    //Variable declaration
+    public SpriteRenderer bomb;
+    public TextMesh number;
+    private int numBombs;
+
+	// Use this for initialization
+	void Start ()
+    {
+        number.text = "";
+        bomb.enabled = false;
+        numBombs = 0;
+	}
+
+    //Set the text to how many bombs surround the current tile
+    public void SetText(int num)
+    {
+        number.text = num.ToString();
+    }
+
+    //Activate the bomb sprite
+    public void SetBomb(bool dead)
+    {
+        if (dead)
+        {
+            bomb.enabled = true;
+        }
+        else
+        {
+            bomb.enabled = false;
+        }
+    }
+
+    //Get the status of bomb.enabled
+    public bool GetBomb()
+    {
+        return bomb.enabled;
+    }
+
+    private void OnMouseDown()
+    {
+        MeshRenderer m = GetComponent<MeshRenderer>();
+        m.enabled = false;
+        SetText(8);
+    }
+}

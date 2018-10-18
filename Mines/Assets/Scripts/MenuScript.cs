fa2168baefaded851ed2c055a4cf26a151e272fc
Michael Bonessi <mbonessi@uncc.edu>
2018-10-18 13:52:55 -0400

    Added Menus

---
+++ Mines/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Canvas mainMenu;
+    public Canvas instructions;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+	
+    public void Instructions()
+    {
+        mainMenu.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        instructions.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(true);
+    }
+}
